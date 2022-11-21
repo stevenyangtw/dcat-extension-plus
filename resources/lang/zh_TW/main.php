27c4ca99ad221135a8bc4981ddb44e37e99166b1
stevenyangtw <81375443+stevenyangtw@users.noreply.github.com>
2022-11-21 15:41:57 +0800

    繁中語系

---
+++ resources/lang/zh_TW/main.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'site_url' => '網站domain',
+    'site_title' => '網站標題',
+    'site_logo_text' => 'LOGO（文字）',
+    'site_logo' => 'LOGO',
+    'site_logo_mini' => 'LOGO（微缩）',
+    'site_debug' => '調試模式',
+    'footer_remove' => '移除底部授權',
+    'select_create' => '快速創建選項',
+    'theme_color' => '主題顏色',
+    'sidebar_style' => '選單樣式',
+    'site_lang' => '語系',
+    'grid_row_actions_right' => '表單行操作按鈕靠右'
+];
