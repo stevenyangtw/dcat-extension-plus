a67a553e153a5fa12d28852e9d00b368207cac66
stevenyangtw <81375443+stevenyangtw@users.noreply.github.com>
2022-11-21 15:35:37 +0800

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-use Celaraze\DcatPlus\Http\Controllers\DcatPlusFieldController;
+//use Celaraze\DcatPlus\Http\Controllers\DcatPlusFieldController;
 use Celaraze\DcatPlus\Http\Controllers\DcatPlusSiteController;
 use Celaraze\DcatPlus\Http\Controllers\DcatPlusUIController;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +10,7 @@ Route::get('/dcat-plus/site', [DcatPlusSiteController::class, 'index'])
 
 Route::get('/dcat-plus/ui', [DcatPlusUIController::class, 'index'])
     ->name('dcat-plus.ui.index');
-
+/*
 Route::get('/dcat-plus/field', [DcatPlusFieldController::class, 'index'])
     ->name('dcat-plus.field.index');
+*/
